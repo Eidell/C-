9d6a5b82f5ad5248a5b0c32438465e3dac33d5ea
Eidell <924751791@qq.com>
2022-11-04 22:50:59 +0800

    一次提交

---
+++ process/mycode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+		int cnt = 10;
+		while(cnt)
+		{
+				printf("剩余时间：%2d\r",cnt);
+				cnt--;
+				fflush(stdout);
+				sleep(1);
+		}
+		return 0;
+}
